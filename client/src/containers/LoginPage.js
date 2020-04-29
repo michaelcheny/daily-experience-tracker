e48773d103f7327f69705392b5042c0767077daa
Michael Chen <michaelchengaming@gmail.com>
2020-04-28 18:31:27 -0700

    delete unnessary boiler plate react junk, begin setting up login stuff

---
+++ client/src/containers/LoginPage.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+
+const LoginPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("email: " + email);
+    console.log("password: " + password);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+        required
+      ></input>
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+        required
+      ></input>
+      <input type="submit" />
+    </form>
+  );
+};
+
+export default LoginPage;
