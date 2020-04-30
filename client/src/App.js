694311889e3445967987d0dda9a156b1f8b55536
Michael Chen <michaelchengaming@gmail.com>
2020-04-29 23:42:41 -0700

    move the fetch auto log in stuff to app root level, pass in context value as object with setuser

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import HomePage from "./containers/HomePage";
@@ -7,9 +7,27 @@ import NavBar from "./components/HomePage/NavBar";
 import { UserContext } from "./context/UserContext";
 
 function App() {
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      fetch("http://localhost:3001/auto_login", {
+        headers: {
+          Authorization: token,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setUser(data);
+          console.log(data);
+        });
+    }
+  }, []);
+
   return (
     <div className="App">
-      <UserContext.Provider value="helloooo">
+      <UserContext.Provider value={{ user, setUser }}>
         <Router>
           <NavBar />
           <Switch>
--- client/src/containers/HomePage.js
+++ client/src/containers/HomePage.js
@@ -9,33 +9,16 @@
 // };
 
 // export default HomePage;
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import { UserContext } from "../context/UserContext";
 
 const HomePage = () => {
-  const fromContext = useContext(UserContext);
-  const [user, setUser] = useState("");
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      fetch("http://localhost:3001/auto_login", {
-        headers: {
-          Authorization: token,
-        },
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          setUser(data);
-          console.log(data);
-        });
-    }
-  }, []);
+  const { user } = useContext(UserContext);
 
   return (
     <div>
       <h1>yoyo</h1>
-      <h5>from context: {fromContext}</h5>
+      <h5>from context: {user.email}</h5>
       <p>{user.email}</p>
     </div>
   );
